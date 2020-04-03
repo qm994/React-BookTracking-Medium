ec0bf436f67526e0225a3f1022c9557534406e2a
Qingyuan Ma <ma@MacBook-Pro-3.fios-router.home>
2020-04-02 22:04:27 -0400

    add CurrentlyReadingComponent

---
+++ src/CurrentlyReading.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+class CurrentlyReading extends React.Component{
+    
+    render(){
+        {console.log(this.props.readings)}
+        return(
+            <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {this.props.readings.map((book) => (
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193,
+                                            backgroundImage: `url(${book.imageLinks.thumbnail})`}}>
+                                        </div>
+                                        <div className="book-shelf-changer">
+                                        <select>
+                                            <option value="move" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
+                                        </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors}</div>
+                                </div>
+                          </li>
+                        ))
+                        }
+                    </ol>
+                  </div>
+                </div>
+        )
+    }
+};
+
+export default CurrentlyReading;
